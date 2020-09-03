32d1fabda7d1f254af26581e74b9fb15c06c5b72
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-03 23:47:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,14 +7,17 @@ class Artist
         @name = name
         @songs = []
     end
+    
     def songs
         Song.all
     end
+    
     def add_song (song)
         @songs << song
         song.artist = self
         @@song_count += 1
     end
+    
     def add_song_by_name (name)
         song = Song.new(name)
         # @songs << song
@@ -22,6 +25,7 @@ class Artist
         song.artist = self
         @@song_count += 1
     end
+    
     def self.song_count
         @@song_count
     end
